993db5914441abae75388cb36afafce392a37365
david_adress <dadress@gmail.com>
2018-12-16 11:49:42 -0500

    added a

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 
 <html>
 <head>	
-	<title>Homepage Test 1.0</title>
+	<title>Homepage Test 1.A</title>
 </head>
 
 <body>

3b6a423e3617a6c87b855ac4a5cdf1d50597592a
david_adress <dadress@gmail.com>
2018-12-16 11:34:29 -0500

    no message

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
 
 <html>
 <head>	
-	<title>Homepage Test</title>
+	<title>Homepage Test 1.0</title>
 </head>
 
 <body>
